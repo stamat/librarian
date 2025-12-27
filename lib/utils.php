9cd17d1ecaab4977cc5557e131befe91dbf9af6f
Nikola Stamatovic <stamatmail@gmail.com>
2025-12-27 12:36:55 +0100

    tabindex lol

--- lib/utils.php
+++ lib/utils.php
@@ -1,14 +1,14 @@
 <?php
 
 function loadini($path, $defaults = []) {
-    if (!file_exists($path)) {
-        return $defaults;
-    }
+  if (!file_exists($path)) {
+    return $defaults;
+  }
 
-    if (!is_readable($path)) {
-        throw new Exception("Cannot read INI file: $path");
-    }
+  if (!is_readable($path)) {
+    throw new Exception("Cannot read INI file: $path");
+  }
 
-    $data = parse_ini_file($path, true);
-    return array_merge($defaults, $data ?: []);
+  $data = parse_ini_file($path, true);
+  return array_merge($defaults, $data ?: []);
 }
